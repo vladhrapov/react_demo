6fb0569dcf9ad23e95fd8cef163ad4fe76fc082d
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2016-01-22 17:58:28 +0200

    Delete unused components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ThumbnailList from "./components/ThumbnailList.jsx";
-import Dropdown from "./components/Dropdown.jsx";
-import { options, options2 } from "./data.js";
 
-console.log(options, options2);
 
-ReactDOM.render(React.createElement(ThumbnailList, options), document.getElementById("target"));
-ReactDOM.render(React.createElement(Dropdown, options2), document.getElementById("dropdown-wrapper"));
+//ReactDOM.render(React.createElement(ThumbnailList, options), document.getElementById("target"));
+//ReactDOM.render(React.createElement(Dropdown, options2), document.getElementById("dropdown-wrapper"));
 //ReactDOM.render(<ThumbnailList  />, document.getElementById("target"));
